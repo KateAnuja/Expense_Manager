b57ba6f0fd2f2ca999c3452da9815edfaa696ba3
Anuja Kate <kate.anuja15@gmail.com>
2021-05-20 22:07:19 +0530

    add transaction to firebase and get from firebase

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Transaction } from '../models/transaction';
+import { TransactionService } from '../services/transaction.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  transactionArray : any= [];
+  constructor(
+    private transactionService : TransactionService,
+  ) {}
 
-  constructor() {}
+  ngOnInit(){
+   this.transactionService.getAllTransaction()
+   .subscribe((transactionArray)=>{
+     this.transactionArray=transactionArray
+     .map(e=>{
+      return {
+        id : e.payload.doc.id,
+        date : e.payload.doc.data()['transactionDate'],
+        amount : e.payload.doc.data()['amount'],
+        category : e.payload.doc.data()['category'],
+        description : e.payload.doc.data()['description']
+      }
+     })
+   })
+    console.log(this.transactionArray);
+  }
 
 }
--- src/app/services/transaction.service.ts
+++ src/app/services/transaction.service.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Transaction } from '../models/transaction';
+import { Constants } from '../util/constants';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionService {
   transactionList : any;
+  
   constructor(
     private fireStore: AngularFirestore
   ) { } 
 
   addTransaction(transaction : Transaction){
     const id = this.fireStore.createId();
-    return this.fireStore.doc(`transaction/${id}`).set({
+    return this.fireStore.doc(Constants.DB_TRANSACTION+"/"+id).set({
       transactionDate : transaction.transactionDate,
       amount : transaction.amount,
       category : transaction.category,
@@ -22,7 +24,6 @@ export class TransactionService {
   }
 
   getAllTransaction(){
-    this.transactionList = this.fireStore.collection<Transaction>('transaction');
-    return this.transactionList;
+    return  this.fireStore.collection<Transaction>(Constants.DB_TRANSACTION).snapshotChanges();
   }
 }
